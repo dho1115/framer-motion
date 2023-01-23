3e50dd54ee216edf9c4c7a65ca5f03f976deee64
DAVID HO <davidhorph@hotmail.com>
2023-01-23 08:40:43 -0600

    Commented out the prv. animate={{}} and <motion.h2></motion.h2> because we do not need it anymore. Those were just examples.

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -4,18 +4,27 @@ import { motion } from 'framer-motion'; //import motion.
 
 const Home = () => {
   return (
-    <div className="home container">
+    <motion.div className="home container"
+      animate={{}}
+      // animate={{marginTop: 150, opacity: 0.3, rotateZ: 185}}
+    >
       {/* See #ToAninmate(start). */}
-      <motion.h2 animate={{fontSize: 53, color: "#ff2994", x: 305, y: 177}}>
+      {/* <motion.h2 animate={{fontSize: 53, color: "#ff2994", x: 17}}>
         Welcome to Pizza Joint
-      </motion.h2>
+      </motion.h2> */}
+      <h2>
+        Welcome to Pizza Joint
+      </h2>
       <Link to="/base">
         {/* See #ToAninmate(start). */}
-        <motion.button>
+        <motion.button
+          // animate={{scale: 1.75}}
+          animate={{}}
+        >
           Create Your Pizza
         </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
 
